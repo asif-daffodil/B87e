c1f9e4201ec8d667036f0e1b6d2265c1b4d446c4
asif-daffodil <abir@dipti.com.bd>
2025-01-12 19:26:46 +0600

    1122025726pm

---
+++ Day18 Jan-12-2025/script.js
@@ -0,0 +1,74 @@
+var myName = "Asif Abir";
+var myCity = "Dhaka";
+var myContact = "01700000000";
+var myWeight = 70;
+var isMarried = true;
+var criminalRecord = null;
+
+console.log(myName + " " + myCity + " " + myContact + " " + myWeight + " " + isMarried + " " + criminalRecord);
+
+// this is a comment
+/**
+ * + Addition
+ * - Subtraction
+ * * Multiplication
+ * / Division
+ * % Modulus
+ */
+
+console.log(5 + 5);
+console.log(5 - 5);
+console.log(5 * 5);
+console.log(5 / 5);
+console.log(5 % 5);
+
+// Increment and Decrement
+/**
+ * ++ Increment
+ * -- Decrement
+ */
+
+var myVar = 87;
+myVar++;
+console.log(myVar);
+
+// Assignment Operators
+/**
+ * = Assignment
+ * += Addition Assignment
+ * -= Subtraction Assignment
+ * *= Multiplication Assignment
+ * /= Division Assignment
+ * %= Modulus Assignment
+ */
+
+var myVar = 5;
+// myVar = myVar + 6;
+myVar += 6;
+myVar -= 4;
+myVar *= 2;
+myVar /= 2;
+myVar %= 2;
+
+// comparison operators
+/**
+ * == Equal to
+ * === Equal value and equal type
+ * != Not equal
+ * !== Not equal value or not equal type
+ * > Greater than
+ * < Less than
+ * >= Greater than or equal to
+ * <= Less than or equal to
+ */
+
+console.log(5 !== "5");
+
+// Logical Operators
+/**
+ * && and
+ * || or
+ * ! not
+ */
+
+console.log(5 > 3 || 5 > 10);
